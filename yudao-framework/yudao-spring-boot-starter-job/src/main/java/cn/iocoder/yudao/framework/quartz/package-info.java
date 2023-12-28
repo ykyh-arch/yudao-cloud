5a3ee03685d95c937f3fc05795a302d6ec012095
guoyukai <643582433@qq.com>
2023-12-28 16:45:52 +0800

    1、解读 starter-job 相关功能

--- yudao-framework/yudao-spring-boot-starter-job/src/main/java/cn/iocoder/yudao/framework/quartz/package-info.java
+++ yudao-framework/yudao-spring-boot-starter-job/src/main/java/cn/iocoder/yudao/framework/quartz/package-info.java
@@ -1,5 +1,5 @@
 /**
  * 1. 定时任务，基于 XXL-Job 实现。
- * 2. 异步任务，采用 Spring Async 异步执行。
+ * 2. 异步任务，采用 Spring Async 异步执行。接入 TransmittableThreadLocal 作为线程池的任务处理方式
  */
 package cn.iocoder.yudao.framework.quartz;
