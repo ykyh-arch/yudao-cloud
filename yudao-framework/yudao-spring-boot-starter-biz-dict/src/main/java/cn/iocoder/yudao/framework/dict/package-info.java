5496fa070877775ef60e72f0791eb5fba2e5ab45
guoyukai <643582433@qq.com>
2024-01-02 16:15:02 +0800

    1、解读 starter-dic 相关功能

--- yudao-framework/yudao-spring-boot-starter-biz-dict/src/main/java/cn/iocoder/yudao/framework/dict/package-info.java
+++ yudao-framework/yudao-spring-boot-starter-biz-dict/src/main/java/cn/iocoder/yudao/framework/dict/package-info.java
@@ -1,6 +1,6 @@
 /**
  * 字典数据模块，提供 {@link cn.iocoder.yudao.framework.dict.core.util.DictFrameworkUtils} 工具类
  *
- * 通过将字典缓存在内存中，保证性能
+ * 通过将字典缓存在内存中，保证性能！通过 guava 提供的缓存机制 LoadingCache，对字典缓存就行自动管理，使用时只需获取数据即可
  */
 package cn.iocoder.yudao.framework.dict;
